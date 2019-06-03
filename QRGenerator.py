01401a6e3892d433524a5736f45aa62bcef8738e
Arunav Gupta <centaurus.ab@gmail.com>
2019-06-03 13:17:01 -0700

    hopefully this works

--- QRGenerator.py
+++ QRGenerator.py
@@ -1,3 +1,3 @@
 import qrcode
-codes = qrcode.make('https://memeteam19.github.io/Senior-Prank19/20190603')
+codes = qrcode.make('https://memeteam19.github.io/Senior-Prank19/?id=20190603')
 codes.save("code2.png")
